cc8dbb129854f77a3bda022e37b0de07e0bb2808
AuroraVeil <Alexpeiser423@gmail.com>
2024-02-12 18:13:41 -0600

    fix targetless crashing

--- Idle.gd
+++ Idle.gd
@@ -2,8 +2,10 @@ extends State
 var move_direction : Vector2 = Vector2.ZERO
 var wander_time : float
 func Update(delta):
-	print(entity.position)
-	if entity.position.distance_to(entity.target.position) < 500 and !entity.target.isInvis:
+	print(entity.target)
+	if entity.target == null:
+		update_wander(delta)
+	elif entity.position.distance_to(entity.target.position) < 500 and !entity.target.isInvis:
 		Transitioned.emit(self, "Aware")
 	elif entity.HP <= 0:
 		var level = entity.get_parent()
@@ -11,13 +13,15 @@ func Update(delta):
 		Transitioned.emit(self, "Dead")
 		entity.anim.play("Death_ani")
 	else:
-		if wander_time > 0:
-			wander_time -= delta
-		else:
-			randomize_wander()
+		update_wander(delta)
 func PhysicsUpdate(delta):
 	entity.velocity = move_direction * delta * 4000
 	entity.move_and_slide()
+func update_wander(delta):
+	if wander_time > 0:
+			wander_time -= delta
+	else:
+		randomize_wander()
 func randomize_wander():
 	move_direction = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
 	wander_time = randf_range(0,1.5)
